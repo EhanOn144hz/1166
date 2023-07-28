c65a209cfda107ca45205719d6a4773897b3b370
EhanOn144hz <88392390+EhanOn144hz@users.noreply.github.com>
2023-07-28 16:57:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload(){
+mustache= loadImage("https://i.postimg.cc/B6KSJfT2/R.png");
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet= ml5.poseNet(video,modelLoaded);
+poseNet.on("pose", gotPoses);
+
+}
+function draw(){
+image(video,0,0,300,300);
+}
+function take_snapshot(){
+    save("image.png");
+}
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y-3;
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+    }
+}
+noseX=0;
+noseY=0;
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache, noseX, noseY, 40,30); 
+}
